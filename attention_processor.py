275eeeb522bb5e5536a714bd3a44bd4a055cc225
vivekdhir77 <vivekdhir77@gmail.com>
2025-02-15 17:15:49 -0500

    added path in attention processor

--- attention_processor.py
+++ attention_processor.py
@@ -1,5 +1,5 @@
 # Copyright 2024 The HuggingFace Team. All rights reserved.
-# /Users/vivekdhir77/Desktop/Latte/venv/lib/python3.13/site-packages
+# /Users/vivekdhir77/Desktop/Latte/venv/lib/python3.13/site-packages/diffusers/models/attention_processor.py
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
